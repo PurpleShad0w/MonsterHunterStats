b55084bdc970aebdb0cf0cbc8cfad8fa099b97f9
Octave <thepurpl3shad0w@gmail.com>
2022-04-27 00:49:12 +0200

    Dictionaries update

--- stories_2_inventory.py
+++ stories_2_inventory.py
@@ -14,9 +14,9 @@ df.set_index('Name', inplace=True)
 
 # Create dataframes
 df2 = pd.DataFrame(data={'ID':0,'Name':0,'Quantity':0,'Category':0,'Subcategory':0},index=(0,1))
-df3 = pd.DataFrame(data={'Index':0,'Type ID':0,'Name':0,'Level':0,'Type':0,'Subcategory':0},index=(0,1))
-df4 = pd.DataFrame(data={'Index':0,'Name':0,'Level':0},index=(0,1))
-df5 = pd.DataFrame(data={'ID':0,'Skill I ID':0,'Skill II ID':0,'Name':0,'Skill I':0,'Skill II':0,'Subcategory':0},index=(0,1))
+df3 = pd.DataFrame(data={'Index':0,'Type ID':0,'Name':0,'Level':0,'Quantity':0,'Type':0,'Subcategory':0},index=(0,1))
+df4 = pd.DataFrame(data={'Index':0,'Name':0,'Level':0,'Quantity':0},index=(0,1))
+df5 = pd.DataFrame(data={'ID':0,'Skill I ID':0,'Skill II ID':0,'Name':0,'Quantity':0,'Skill I':0,'Skill II':0,'Subcategory':0},index=(0,1))
 
 # Locate items, weapons, armor and talismans
 index_item = df.index.get_loc('struct item i[1999]')
@@ -47,7 +47,7 @@ for i in range(0,4200):
             index = df_weapons.iloc[i,1]
         if 'Weapon Level' in df_weapons.iloc[i,0]:
             level = df_weapons.iloc[i,1]
-            s = {'Index':index,'Type ID':type_id,'Name':0,'Level':level,'Type':type,'Subcategory':0}
+            s = {'Index':index,'Type ID':type_id,'Name':0,'Level':level,'Quantity':1,'Type':type,'Subcategory':0}
             df3 = df3.append(s,ignore_index=True)
 
 # Gather armor
@@ -56,7 +56,7 @@ for i in range(0,9600):
             index = df_armor.iloc[i,1]
         if 'Armor Level' in df_armor.iloc[i,0]:
             level = df_armor.iloc[i,1]
-            s = {'Index':index,'Name':0,'Level':level}
+            s = {'Index':index,'Name':0,'Level':level,'Quantity':1}
             df4 = df4.append(s,ignore_index=True)
 
 # Gather talismans
@@ -69,7 +69,7 @@ for i in range(0,3001):
         if 'Skill ID 2' in df_talisman.iloc[i,0]:
             skill_2 = df_talisman.iloc[i,1]
             skill_2_id = re.sub('[^0-9]','',skill_2)
-            s = {'ID':id,'Skill I ID':skill_1_id,'Skill II ID':skill_2_id,'Name':0,'Skill I':skill_1,'Skill II':skill_2,'Subcategory':0}
+            s = {'ID':id,'Skill I ID':skill_1_id,'Skill II ID':skill_2_id,'Name':0,'Quantity':1,'Skill I':skill_1,'Skill II':skill_2,'Subcategory':0}
             df5 = df5.append(s,ignore_index=True)
 
 # Cleaning dataframes
@@ -99,7 +99,7 @@ for i in range(len(df3)):
         df_temp = df_dict_weapons[(df_dict_weapons['Index'] == int(float(index))) & (df_dict_weapons['Type ID'] == int(float(type_id)))]
         try:
             df_temp.reset_index(inplace=True)
-            s = {'Index':index,'Type ID':type_id,'Name':df_temp.loc[0,'Name'],'Level':0,'Type':0,'Subcategory':df_temp.loc[0,'Subcategory']}
+            s = {'Index':index,'Type ID':type_id,'Name':df_temp.loc[0,'Name'],'Level':0,'Quantity':0,'Type':0,'Subcategory':df_temp.loc[0,'Subcategory']}
             df3 = df3.append(s,ignore_index=True)
         except KeyError:
             continue
@@ -108,29 +108,28 @@ for i in range(len(df3)):
 for i in range(len(df4)):
     index = df4.iloc[i,0]
     try:
-        s = {'Index':index,'Name':df_dict_armor.loc[int(float(index)),'Name'],'Level':0}
+        s = {'Index':index,'Name':df_dict_armor.loc[int(float(index)),'Name'],'Level':0,'Quantity':0}
         df4 = df4.append(s,ignore_index=True)
     except KeyError:
         continue
 
 # Adding talismans information
 for i in range(len(df5)):
-        id = df5.iloc[i,0]
         skill_1_id = df5.iloc[i,1]
         skill_2_id = df5.iloc[i,2]
-        df_temp = df_dict_talismans[(df_dict_talismans['ID'] == int(float(id))) & (df_dict_talismans['Skill I ID'] == int(float(skill_1_id))) & (df_dict_talismans['Skill II ID'] == int(float(skill_2_id)))]
+        df_temp = df_dict_talismans[(df_dict_talismans['Skill I ID'] == int(float(skill_1_id))) & (df_dict_talismans['Skill II ID'] == int(float(skill_2_id)))]
         try:
             df_temp.reset_index(inplace=True)
-            s = {'ID':id,'Skill I ID':skill_1_id,'Skill II ID':skill_2_id,'Name':df_temp.loc[0,'Name'],'Skill I':0,'Skill II':0,'Subcategory':df_temp.loc[0,'Subcategory']}
+            s = {'ID':0,'Skill I ID':skill_1_id,'Skill II ID':skill_2_id,'Name':df_temp.loc[0,'Name'],'Quantity':0,'Skill I':0,'Skill II':0,'Subcategory':df_temp.loc[0,'Subcategory']}
             df5 = df5.append(s,ignore_index=True)
         except KeyError:
             continue
 
 # Cleaning
 df2 = df2.groupby(df2['ID'],sort=False).aggregate({'Name':'last','Quantity':'first','Category':'last','Subcategory':'last'})
-df3 = df3.groupby([df3['Index'],df3['Type ID']],sort=False).aggregate({'Name':'last','Level':'first','Type':'first','Subcategory':'last'})
-df4 = df4.groupby(df4['Index'],sort=False).aggregate({'Name':'last','Level':'first'})
-df5 = df5.groupby([df5['ID'],df5['Skill I ID'],df5['Skill II ID']],sort=False).aggregate({'Name':'last','Skill I':'first','Skill II':'first','Subcategory':'last'})
+df3 = df3.groupby([df3['Index'],df3['Type ID']],sort=False).aggregate({'Name':'last','Level':'first','Quantity':'sum','Type':'first','Subcategory':'last'})
+df4 = df4.groupby(df4['Index'],sort=False).aggregate({'Name':'last','Level':'first','Quantity':'sum'})
+df5 = df5.groupby([df5['Skill I ID'],df5['Skill II ID']],sort=False).aggregate({'ID':'first','Name':'last','Quantity':'sum','Skill I':'first','Skill II':'first','Subcategory':'last'})
 
 # Outputting dataframes
 df2.to_csv(r'stories_2_output_items.csv',encoding='utf-8')
